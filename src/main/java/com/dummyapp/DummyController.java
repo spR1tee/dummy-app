f242b35ef3befadc01222f1d64a9f23d203dbdc4
Eliot Nagy <nagyeliot@gmail.com>
2024-12-16 19:56:28 +0100

    add controller and data structure changes

---
+++ src/main/java/com/dummyapp/DummyController.java
@@ -0,0 +1,20 @@
+package com.dummyapp;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dummy-app")
+public class DummyController {
+
+    @PostMapping("/receiveData")
+    public ResponseEntity<String> receiveData(@RequestBody String data) {
+        // Process the received data
+        System.out.println(data);
+        return ResponseEntity.ok("Data received successfully");
+    }
+}
+
--- src/main/java/com/dummyapp/DummyService.java
+++ src/main/java/com/dummyapp/DummyService.java
@@ -10,16 +10,16 @@ import org.springframework.web.client.RestTemplate;
 import java.io.*;
 
 @Service
-public class SimulationService {
+public class DummyService {
 
     private final RestTemplate restTemplate = new RestTemplate();
     private final HttpHeaders headers = new HttpHeaders();
 
-    public SimulationService() {
+    public DummyService() {
         headers.setContentType(MediaType.APPLICATION_JSON);
     }
 
-    @Scheduled(fixedRate = 5000) // időzítés: 5mp
+    @Scheduled(fixedRate = 5000) // timing: 5 sec
     public void sendRequest() {
         try {
             File jsonInputString = new File("src\\main\\resources\\data\\vmdata.json");
@@ -32,16 +32,11 @@ public class SimulationService {
             br.close();
 
             HttpEntity<String> request = new HttpEntity<>(jsonContent.toString(), headers);
-            String apiUrl = "http://localhost:8080/api/startSimulation";
+            String apiUrl = "http://localhost:8080/simulator/request";
             String response = restTemplate.postForObject(apiUrl, request, String.class);
             System.out.println("Response from API: " + response);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // Kérés a válasz végpontra
-        /*String statusApiUrl = "http://localhost:8080/api/simulationStatus";
-        String statusResponse = restTemplate.getForObject(statusApiUrl, String.class);
-        System.out.println("Simulation status from API: " + statusResponse);*/
     }
 }
