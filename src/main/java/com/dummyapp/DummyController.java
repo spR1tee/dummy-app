2ff61e20044979ae25bba264cea7105e0444c53a
Eliot Nagy <nagyeliot@gmail.com>
2025-03-03 20:00:45 +0100

    changes in file structures and minor changes for accepting data from digital twin

--- src/main/java/com/dummyapp/DummyController.java
+++ src/main/java/com/dummyapp/DummyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/dummy-app")
+@RequestMapping("/dummy")
 public class DummyController {
 
     @PostMapping("/receiveData")
